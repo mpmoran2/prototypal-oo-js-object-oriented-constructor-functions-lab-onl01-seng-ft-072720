b32639f8fab38964ba6ab38ccd59a7544ff4bb93
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-13 17:55:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 //Scooter with `year`, `color`, and `model` properties
 
+function Scooter(year, color, model){
+  this.year = year;
+  this.color = color;
+  this.model = model
+}
 
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(address, city){
+  this.address = address;
+  this.city = city;
+}
 
 //Driver with `name`, `age`, and `experience` properties
 
